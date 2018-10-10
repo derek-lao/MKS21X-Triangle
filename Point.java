0e13ddba01ecb5a43389eb5dcbdbd064efb84fd6
Derek Lao <dlao00@stuy.edu>
2018-10-10 01:14:35 -0400

    for triangle class, have everything but toString, setVertex.  for point class, have everything but toString

---
+++ Point.java
@@ -0,0 +1,32 @@
+public class Point{
+  private double xcor;
+  private double ycor;
+
+  public Point(double x, double y){
+    xcor=x;
+    ycor=y;
+  }
+
+  public double getXcor(){
+    return xcor;
+  }
+
+  public double getYcor(){
+    return ycor;
+  }
+
+  public double distanceTo(Point newpoint){
+    return Math.sqrt((this.getXcor() - newpoint.getXcor())
+          *(this.getXcor() - newpoint.getXcor()) +
+           (this.getYcor() - newpoint.getYcor())
+          *(this.getYcor() - newpoint.getYcor()));
+  }
+
+  public double distanceTo(Point point1, Point point2){
+    return Math.sqrt((point1.getXcor() - point2.getXcor())
+          *(point1.getXcor() - point2.getXcor()) +
+           (point1.getYcor() - point2.getYcor())
+          *(point1.getYcor() - point2.getYcor()));
+  }
+
+}
---
+++ Triangle.java
@@ -0,0 +1,36 @@
+public class Triangle{
+  private Point pointA;
+  private Point pointB;
+  private Point pointC;
+
+  public Triangle(Point newpointA,
+                  Point newpointB,Point newpointC){
+    pointA=newpointA;
+    pointB=newpointB;
+    pointC=newpointC;
+  }
+
+  public Triangle(double axcor, double aycor, double bxcor, double bycor,
+                  double cxcor, double cycor){
+                    Point pointA=new Point(axcor,aycor);
+                    Point pointB=new Point(bxcor,bycor);
+                    Point pointC=new Point(cxcor,cycor);
+  }
+
+  public double getPerimeter(){
+    return pointA.distanceTo(pointB) +
+           pointB.distanceTo(pointC) + pointC.distanceTo(pointA);
+  }
+
+  public Point getVertex(int index){
+    Point[] pointarray = {pointA, pointB, pointC};
+    /* Point[] pointarray=new Point[3];
+    pointarray[0]=pointA;
+    pointarray[1]=pointB;
+    pointarray[2]=pointC;  */
+    Point pointcopy=new Point(pointarray[index].getXcor(),
+                                pointarray[index].getYcor());
+    return pointcopy;
+  }
+
+}
